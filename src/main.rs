95fbfce6dbd87e314a1df78e46008a865676267d
Colin Cook <cook.colin13@gmail.com>
2021-07-29 15:12:28 -0500

    added comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// How to add a task
+//cargo run -p backend --bin todo new 'task_name'
+
+//How to start the frontend/backend
+// cargo run -p backend --bin backend
+// cargo run -p frontend --bin frontend
+
+//How to build the whole project
+//cargo make
